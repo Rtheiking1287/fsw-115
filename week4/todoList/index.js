76eac9d04d4f94436d45c993e7b58ce9ab6c6e40
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-07-14 23:02:43 -0700

    Add files via upload

--- week4/todoList/index.js
+++ week4/todoList/index.js
@@ -3,9 +3,14 @@
 axios.get('http://api.bryanuniversity.edu/bobby_heiking/list')
 .then(response => {
     for(let i = 0; i < response.data.length; i++){
-        const h2 = document.createElement('h2')
-        h2.textContent = response.data[i].name
-        document.body.appendChild(h2)
+        const listItem = document.createElement('h2')
+        listItem.textContent = response.data[i].name
+
+        if(response.data[i].isComplete === true){
+            listItem.classList.add('is-completed')
+        }
+        
+        document.body.appendChild(listItem)
     }
 })
 .catch(error => console.log(error))
